5c838a4dafebc8e23d1ace61d0b5e050fa332a74
pc <pc@DESKTOP-90C2CNI>
2019-03-29 15:29:18 +0800

    opotimize

--- IntentConnectWeighing/Helper/DatabaseOPtionHelper.cs
+++ IntentConnectWeighing/Helper/DatabaseOPtionHelper.cs
@@ -1,26 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using MyHelper;
-using System.Data;
+using SqlDao;
 namespace IntentConnectWeighing
 {
     public class DatabaseOPtionHelper
     {
-        private static SqlDao.DbHelper Instance;    
+        
+        private static DbHelper Instance;    
         private static String dbType = ConfigurationHelper.GetConfig(ConfigItemName.dbType.ToString());
         public static string connStrTemplate = " Data Source={0};Version=3;Pooling=False;Max Pool Size=100;";
-        public static SqlDao.DbHelper GetInstance()
+        public static DbHelper GetInstance()
         {
             if (Instance == null)
             {
                 if (dbType == DbType.mysql.ToString()) {                    
-                    Instance = new SqlDao.MySqlHelper(ConfigurationHelper.GetConnectionConfig(ConfigItemName.mysqlConn.ToString()));
+                    Instance = new MySqlHelper(ConfigurationHelper.GetConnectionConfig(ConfigItemName.mysqlConn.ToString()));
                 } else if(dbType == DbType.sqlite.ToString()) {
-                    Instance = new SqlDao.SQLiteHelper(ConfigurationHelper.GetConnectionConfig(ConfigItemName.sqliteConn.ToString()));
+                    Instance = new SQLiteHelper(ConfigurationHelper.GetConnectionConfig(ConfigItemName.sqliteConn.ToString()));
                 }               
             }
             return Instance;
